60662fca72fc7edc8498928b4ffb845d9ab7bbf2
Lee TaeWoo <syr@meu.works>
2022-01-29 20:38:56 +0900

    move utils to another project

--- src/utils/http/mod.rs
+++
@@ -1,7 +0,0 @@
-mod cookie;
-mod header;
-mod set_cookie;
-
-pub use cookie::Cookie;
-pub use header::SetHeaders;
-pub use set_cookie::{SetCookie, SetCookieOptions};
--- src/utils/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod r#async;
-pub mod http;
-
-pub mod jwt;
-mod read_chunks;
-
-pub use read_chunks::ReadChunks;
