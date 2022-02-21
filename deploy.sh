32ace82877d7aed32eb8888234f1bafacf34cbe7
Lee TaeWoo <syr@meu.works>
2022-02-21 12:18:22 +0900

    update deploy scritps

--- deploy.sh
+++ deploy.sh
@@ -1,46 +1,51 @@
-# 개발 환경과 운영 환경을 분리
-# branch로 구분하자
-# 개발 환경일 때는 ./target/x86_64-unknown-linux-musl/release/madome-auth
-# 운영 환경일 때는 ./bin/linux-x86_64/$VERSION
-# 이미지 버전도 개발 환경일 때는 latest, 운영 환경일 때는 $VERSION
-# 운영 환경일 때는 도커 이미지 중에 해당 버전이 있을 경우에는 도커 빌드는 안함
-# 개발 환경일 때는 무조건 함
+SVC=auth
 
-# development mode
-if [ "$(git branch --show-current)" = "release" ]; then
-    VERSION="$(cat Cargo.toml | grep 'version = ' | head -1 | sed -e 's/version = //' | sed -e 's/\"//g')"
+CURRENT_BRANCH="$(git branch --show-current)"
+BIN="./bin/${CURRENT_BRANCH}-$VERSION"
 
-    if [ $? -ne 0 ]; then
-        echo "failed parsing versio from Cargo.toml"
-        exit 1
-    fi
+VERSION="$(cat Cargo.toml | grep 'version = ' | head -1 | sed -e 's/version = //' | sed -e 's/\"//g')"
 
-    BIN="./bin/linux-x86_64/$VERSION"
+if [ $? -ne 0 ]; then
+    echo "failed parsing versio from Cargo.toml"
+    exit 1
+fi
 
-    if [ ! -f $BIN ]; then
-        echo "binary file download"
-        $PWD/update.sh
-    fi
-
-    if [ ! -f $BIN ]; then
-        echo "binary file does not released or not found"
-        exit 1
-    fi
-else
+if [ "$CURRENT_BRANCH" != "stable" ] || [ "$CURRENT_BRANCH" != "beta" ]; then
     cargo build --target=x86_64-unknown-linux-musl
 
     if [ $? -ne 0 ]; then
         exit 1
     fi
 
+    kubectl apply -f k8s_node_port.yml
+
     VERSION="latest"
 
-    BIN="./target/x86_64-unknown-linux-musl/debug/madome-auth"
+    BIN="./target/x86_64-unknown-linux-musl/debug/madome-$SVC"
+else
+    if [ ! -f $BIN ]; then
+        echo "binary file download"
+        ./update.sh
+    fi
+
+    if [ ! -f $BIN ]; then
+        echo "binary file does not released or not found"
+        exit 1
+    fi
+
+    kubectl apply -f k8s_cluster_ip.yml
+
+    if [ ! -f $BIN ]; then
+        exit 1
+    fi
+
+    # e.g. stable-0.1.1
+    VERSION="${CURRENT_BRANCH}-$VERSION"
 fi
 
 chmod +x $BIN
 
-docker build --build-arg BINARY_FILE="$BIN" --tag "madome-auth:$VERSION" . # --no-cache --rm --force-rm
+docker build --build-arg BINARY_FILE="$BIN" --tag "madome-user:$VERSION" .
 
 if [ $? -ne 0 ]; then
     echo "failed docker build"
@@ -49,7 +54,6 @@ fi
 
 cat k8s_deployment.yml | \
 sed -e "s/{VERSION}/$VERSION/g" | \
-# sed -e "s%{WORK_DIR}%$PWD%g" | \
 kubectl apply -f -
 
 if [ $? -ne 0 ]; then
@@ -57,6 +61,6 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-if [ "$(git branch --show-current)" != "release" ]; then
-    kubectl rollout restart deployment/madome-auth
+if [ "$CURRENT_BRANCH" != "stable" ] || [ "$CURRENT_BRANCH" != "beta" ]; then
+    kubectl rollout restart deployment/madome-$SVC
 fi
